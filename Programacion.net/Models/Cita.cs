2cc40bf969619ab8aa62b974a86c29c299f322ca
ssj12 <ssj12@RAIDER>
2025-05-30 16:33:06 -0400

    cummit

---
+++ Programacion.net/Models/Cita.cs
@@ -0,0 +1,16 @@
+﻿namespace Programacion.net.Models
+{
+    public class Cita
+    {
+        public int Id { get; set; }
+        public int clienteId { get; set; }
+        public int ServicioId { get; set; }
+        public DateTime fecha { get; set; }
+
+        public TimeSpan hora { get; set; }
+
+        public string estado { get; set; } // "Pendiente", "Confirmada", "Cancelada"
+
+        public string comentarios { get; set; } // Comentarios adicionales sobre la cita 
+    }
+}
---
+++ Programacion.net/Models/Servicio.cs
@@ -0,0 +1,12 @@
+﻿namespace Programacion.net.Models
+{
+    public class Servicio
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public decimal Precio { get; set; }
+        public DateTime FechaCreacion { get; set; } = DateTime.Now;
+        public bool Activo { get; set; } = true;
+    }
+}
